352b666c2d35e24170c9bc271c1dfc3cae928e70
leandrocunha526 <leandrocunha016@gmail.com>
2022-02-13 03:58:37 -0300

    Update swagger config

--- src/main.ts
+++ src/main.ts
@@ -14,10 +14,9 @@ async function bootstrap() {
     );
 
     const config = new DocumentBuilder()
-        .setTitle('Cats example')
-        .setDescription('The cats API description')
+        .setTitle('API')
+        .setDescription('The API description')
         .setVersion('1.0')
-        .addTag('cats')
         .build();
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
